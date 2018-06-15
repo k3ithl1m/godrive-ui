8ae8cdc6f251c21c26b86b6e75c35460687b11a8
Keith lim <keithlimweiluen@gmail.com>
2018-06-15 04:57:31 -0400

    button

---
+++ src/button/HugeButton.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+import {
+  View,
+  TouchableOpacity,
+  Text,
+  StyleSheet,
+  Dimensions
+} from 'react-native';
+import {GDText} from '../text';
+
+const SCREEN_WIDTH = Dimensions.get("window").height;
+const SCREEN_HEIGHT = Dimensions.get("window").width;
+
+class HugeButton extends Component {
+  render() {
+    const {
+      children,
+      expand,
+      onPress
+    } = this.props;
+
+    return (
+        <TouchableOpacity onPress={onPress} style = {styles.button}>
+          <GDText header textStyle = {{color: "#FFF"}}>{children}</GDText>
+        </TouchableOpacity>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+
+  button: {
+    height: SCREEN_HEIGHT / 6,
+    backgroundColor: "#499333",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+})
+
+export default HugeButton;
